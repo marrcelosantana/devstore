533ea90fba25ddc7df63671eb4acf80898d906b1
marrcelosantana <marcelosantana@alu.ufc.br>
2024-07-29 10:17:30 -0300

    add cypress

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,6 @@
+describe('add prodcut to cart', () => {
+  it('should be able to navigate to the product page and add it to the car', () => {
+    cy.visit('http://localhost:3000')
+    // cy.get('a[hrefˆ="/product"]').first().click()
+  })
+})
